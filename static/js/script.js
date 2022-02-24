71f07d9e82e68b60ff48fda6a25447344f2e0283
ukpatell <umangpatelr4@gmail.com>
2022-02-23 22:10:26 -0500

    Cat Generator Added

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,30 @@
 // #1 - Age in Days
 
-function ageInDays(){   
-    var birthYear = prompt("What year were you born?"); // Input
-    currentYear = new Date().getFullYear();             // Returns the current year
-    var age = (currentYear-birthYear)*365;
-    var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode('You are ' + age + ' days old');
-    h1.setAttribute('id','ageInDays');
-    h1.appendChild(textAnswer);
-    document.getElementById('flex-box-result').appendChild(h1);
+function ageInDays(){
+        currentYear = new Date().getFullYear();             // Returns the current year
+        var birthYear = prompt("What year were you born?"); // Input
+        if(birthYear < 0 || birthYear > currentYear){
+                alert('Invalid Entry. Retr')
+                reset();
+            }
+        var age = (currentYear-birthYear)*365;
+        var h1 = document.createElement('h1');
+        var textAnswer = document.createTextNode('You are ' + age + ' days old');
+        h1.setAttribute('id','ageInDays');
+        h1.appendChild(textAnswer);
+        document.getElementById('flex-box-result').appendChild(h1);
 }
 
 function reset(){
     document.getElementById('ageInDays').remove();
+}
+function resetImage(){
+    document.getElementById("flex-animal-gen").innerHTML = ''
+}
+function generateAnimal(){
+    var image = document.createElement('img');
+    var div = document.getElementById("flex-animal-gen")
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif"
+    div.appendChild(image);
+
 }
